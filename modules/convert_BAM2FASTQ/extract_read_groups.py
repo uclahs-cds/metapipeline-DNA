3451fb2e8dd032133188b47b424cb52220b87cb2
zhuchcn <ChenghaoZhu@mednet.ucla.edu>
2021-11-17 11:38:16 -0800

    fix (BAM2FASTQ): enable using ID for PU

--- modules/convert_BAM2FASTQ/extract_read_groups.py
+++ modules/convert_BAM2FASTQ/extract_read_groups.py
@@ -40,10 +40,15 @@ def parse_args() -> argparse.Namespace:
         metavar='<value>',
         default=None
     )
+    parser.add_argument(
+        '--id-as-pu',
+        action='store_true',
+        help='Use the ID value for PU tag'
+    )
     return parser.parse_args()
 
 def create_read_groups(bam:Path, output:Path, sequencing_center:str,
-        platform_unit:str) -> None:
+        platform_unit:str, id_for_pu:bool) -> None:
     """ Create a read group CSV file from a BAM """
     bam_file = pysam.AlignmentFile(bam, mode='r')
     header = bam_file.header
@@ -80,8 +85,11 @@ def create_read_groups(bam:Path, output:Path, sequencing_center:str,
                     row.append(str(i))
                 elif field == 'sequencing_center' and sequencing_center:
                     row.append(sequencing_center)
-                elif field == 'platform_unit' and platform_unit:
-                    row.append(platform_unit)
+                elif field == 'platform_unit' and platform_unit or id_for_pu:
+                    if id_for_pu:
+                        row.append(read_group['read_group_identifier'])
+                    else:
+                        row.append(platform_unit)
                 else:
                     row.append(read_group[fields_map[field]])
             handle.write(','.join(row) + '\n')
@@ -94,7 +102,8 @@ def main():
             bam=args.input_bam,
             output=args.output_csv,
             sequencing_center=args.sequencing_center,
-            platform_unit=args.platform_unit
+            platform_unit=args.platform_unit,
+            id_for_pu=args.id_for_pu
         )
     except ValueError as error:
         if error.args[0] == BAM_HAS_NO_RG_ERROR:
