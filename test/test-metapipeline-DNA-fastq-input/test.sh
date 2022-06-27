c19f3fdbf4a0ec5799f5c2672106d7a636ff42ad
whelena <hwinata@mednet.ucla.edu>
2022-06-27 13:58:03 -0700

    remove testing script

--- test/test-metapipeline-DNA-fastq-input/test.sh
+++
@@ -1,7 +0,0 @@
-#! bin/bash
-
-NXF_WORK=./test/work nextflow run main.nf \
--c test/global.config \
--c test/test-metapipeline-DNA-fastq-input/test.config \
-# -params-file test/test-metapipeline-DNA-fastq-input/input.yaml \
-> test/test-metapipeline-DNA-fastq-input/test.out
